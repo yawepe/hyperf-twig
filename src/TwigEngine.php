182ab962ec7c406b0f374b9ab48186891a3ea682
ywp <yawepe@qq.com>
2021-08-12 15:20:38 +0800

    210810

--- src/TwigEngine.php
+++ src/TwigEngine.php
@@ -9,12 +9,12 @@ class TwigEngine implements EngineInterface {
         $template = str_ireplace('.','/',$template) . env('TWIG_SUBFFIX','.twig');
         $loader = new FilesystemLoader($config['view_path']);
         $twig = new Environment($loader, [
-            'cache' => $config['cache_path'],
+            'cache' => env('TWIG_CACHE') ?? $config['cache_path'],
             'debug' => env('TWIG_DEBUG',false),
             'charset' => env('TWIG_CHARSET','UTF-8'),
             'strict_variables' => env('TWIG_STRICT_VARIABLES',false),
             'autoescape' => env('TWIG_AUTOESCAPE','html'),
-            'auto_reload' => env('TWIG_AUTO_RELOAD',null),
+            'auto_reload' => env('TWIG_AUTO_RELOAD'),
             'optimizations' => env('TWIG_OPTIMIZATIONS',-1),
 
         ]);
