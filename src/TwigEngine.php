9c869d2d6a47ff5674f0deb39e0c4520cdc0953d
ywp <yawepe@qq.com>
2021-08-12 10:34:50 +0800

    210811

---
+++ src/TwigEngine.php
@@ -0,0 +1,23 @@
+<?php
+namespace Yapf\HyperfTwig;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Hyperf\View\Engine\EngineInterface;
+
+class TwigEngine extends EngineInterface {
+    public function render($template, $data, $config):string{
+        $template = str_ireplace('.','/',$template) . env('subffix','.twig');
+        $loader = new FilesystemLoader($config['view_path']);
+        $twig = new Environment($loader, [
+            'cache' => $config['cache_path'],
+            'debug' => env('debug',false),
+            'charset' => env('charset','UTF-8'),
+            'strict_variables' => env('strict_variables',false),
+            'autoescape' => env('autoescape','html'),
+            'auto_reload' => env('auto_reload',null),
+            'optimizations' => evn('optimizations',-1),
+
+        ]);
+        return $twig->render($template, $data);
+    }
+}
